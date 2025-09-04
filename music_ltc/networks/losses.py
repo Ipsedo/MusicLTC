need fix variance

music_ltc/networks/losses.py
@@ -1,21 +1,13 @@
 import torch as th
 
 
-def normal_kl_div(
-    mu_1: th.Tensor,
-    var_1: th.Tensor,
-    mu_2: th.Tensor,
-    var_2: th.Tensor,
+def var_kl_div(
+    var_pred: th.Tensor,
+    var_target: th.Tensor,
     epsilon: float = 1e-12,
 ) -> th.Tensor:
-    return th.sum(
-        th.log(var_2 + epsilon) / 2.0
-        - th.log(var_1 + epsilon) / 2.0
-        + (var_1 + th.pow(mu_1 - mu_2, 2.0)) / (2 * var_2 + epsilon)
-        - 0.5,
-        dim=-1,
-    )
+    return 0.5 * ((var_target / (var_pred + epsilon)) - 1 - th.log(var_target / (var_pred + epsilon)))
 
 
 def mse(p: th.Tensor, q: th.Tensor) -> th.Tensor:
-    return th.pow(p - q, 2.0).sum(dim=-1)
+    return th.pow(p - q, 2.0)

music_ltc/train.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from .data.dataset import AudioDataset
-from .networks.losses import mse, normal_kl_div
+from .networks.losses import mse, var_kl_div
 from .options import ModelOptions, TrainOptions
 from .saver import AudioSaver, SaveManager, TorchSaver
 
@@ -88,16 +88,14 @@ def train_model(
                 x_t, eps = noiser(x_0, t)
                 eps_theta, v_theta = denoiser(x_t, t)
 
-                loss_mse = mse(eps, eps_theta)
+                loss_mse = mse(eps, eps_theta).mean()
 
-                q_mu, q_var = noiser.posterior(x_t, x_0, t)
-                p_mu, p_var = denoiser.prior(
-                    x_t, t, eps_theta.detach(), v_theta
-                )
+                _, q_var = noiser.posterior(x_t, x_0, t)
+                _, p_var = denoiser.prior(x_t, t, eps_theta, v_theta)
 
-                loss_kl = normal_kl_div(q_mu, q_var, p_mu, p_var)
+                loss_kl = var_kl_div(p_var, q_var).mean()
 
-                loss = th.mean(loss_kl + loss_mse)
+                loss = loss_kl + loss_mse
 
                 optim.zero_grad(set_to_none=True)
                 loss.backward()
@@ -108,16 +106,16 @@ def train_model(
                 tqdm_bar.set_description(
                     f"Epoch {e} / {train_options.epochs - 1} - "
                     f"loss = {loss.item():.6f}, "
-                    f"mse = {loss_mse.mean().item():.6f}, "
-                    f"kl = {loss_kl.mean().item():.6f}, "
+                    f"mse = {loss_mse.item():.6f}, "
+                    f"kl = {loss_kl.item():.6f}, "
                     f"grad_norm = {grad_norm:.6f}"
                 )
 
                 mlflow.log_metrics(
                     {
                         "loss": loss.item(),
-                        "mse": loss_mse.mean().item(),
-                        "kl": loss_kl.mean().item(),
+                        "mse": loss_mse.item(),
+                        "kl": loss_kl.item(),
                         "grad_norm": grad_norm,
                     },
                     step=e * nb_batches + i,